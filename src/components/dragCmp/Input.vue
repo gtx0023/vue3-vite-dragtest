6f9cddca44cc055b736566d0011b7f2e854a0601
caichenlong <caichenlong062@chinasoftinc.com>
2023-07-24 19:29:52 +0800

    feat: 组合式函数

--- src/components/dragCmp/Input.vue
+++ src/components/dragCmp/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import {onMounted, ref, watch} from 'vue';
 import {itemProps} from '@/core/cmpBase'
 const emit = defineEmits(['inFocus', 'submit'])
 const props = defineProps({
@@ -16,12 +16,32 @@ function buttonClick() {
 
 onMounted(()=>{
 })
+
+watch(props,()=>{
+      console.log('props change')
+      getStyle();
+    },
+    { deep: true }
+)
+
+function getStyle(){
+  return {
+    left: `${props.x}px`,
+    top: `${props.y}px`,
+    width: `${props.width}px`,
+    height: `${props.height}px`
+  }
+}
 </script>
 
 <template>
-    <div>
+    <div class="editor-input" :style="getStyle">
         <input type="text" @click="buttonClick" :placeholder="placeholder">
     </div>
 </template>
 
-<style lang="less"></style>
+<style lang="less">
+.editor-input{
+  position: absolute;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { createApp, onMounted, ref } from 'vue';
 import Input from '@/components/dragCmp/Input.vue';
+import {useMouse} from '@/core/cmpBase'
 
 const mainEditor = ref<Element | null>(null)
+const {x,y} = useMouse()
 
 onMounted(() => {
   const cmpIns = createApp(Input, {
-    x: 10,
-    y: 10,
+    x: x.value,
+    y: y.value,
     placeholder: '1111111',
   })
   console.log('--------', cmpIns);
